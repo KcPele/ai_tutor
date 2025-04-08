6b3ef29bcd83ec21494834951a9bed22f4f969de
KcPele <fidekg123@gmail.com>
2025-04-08 19:13:34 +0100

    deployment v2

---
+++ constants/index.ts
@@ -0,0 +1,24 @@
+export const SubscriptionManager_CONTRACT_ADDRESS =
+  "0x0000000000000000000000000000000000000000";
+export const AchievementNFT_CONTRACT_ADDRESS =
+  "0x0000000000000000000000000000000000000000";
+export const StudySessionLogger_CONTRACT_ADDRESS =
+  "0x0000000000000000000000000000000000000000";
+
+export const ContentFeedback_CONTRACT_ADDRESS =
+  "0x0000000000000000000000000000000000000000";
+
+export const ContentFeedback_ABI = import(
+  "../tutor_contract/artifacts/contracts/ContentFeedback.sol/ContentFeedback.json"
+);
+export const SubscriptionManager_ABI = import(
+  "../tutor_contract/artifacts/contracts/SubscriptionManager.sol/SubscriptionManager.json"
+);
+
+export const AchievementNFT_ABI = import(
+  "../tutor_contract/artifacts/contracts/AchievementNFT.sol/AchievementNFT.json"
+);
+
+export const StudySessionLogger_ABI = import(
+  "../tutor_contract/artifacts/contracts/StudySessionLogger.sol/StudySessionLogger.json"
+);
